64453888e5fc6240f5617a5b0960e7a1491346c8
huyq <965672903@qq.com>
2023-03-12 09:30:47 +0800

    1

--- lab04-validation/demo/src/main/java/cn/hyqup/demo/controller/IndexController.java
+++ lab04-validation/demo/src/main/java/cn/hyqup/demo/controller/IndexController.java
@@ -1,17 +1,22 @@
 package cn.hyqup.demo.controller;
 
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hyqup.demo.bean.request.StudentRequest;
 import cn.hyqup.demo.bean.request.TeacherRequest;
 import cn.hyqup.validator.annations.ValidateParam;
 import cn.hyqup.validator.enums.CheckType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @RequestMapping("/index")
 @RestController
 public class IndexController {
     @GetMapping("/hello")
     public String index() {
-
+        log.info("hello");
+        long delay = RandomUtil.randomLong(100, 1000);
+        ThreadUtil.sleep(delay);
         return "hello";
     }
 
